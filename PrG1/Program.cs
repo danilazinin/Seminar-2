00c73ba0379d632caecb4bc12251b9cec1c33eb1
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-02 20:28:51 +0300

    добавили первую программу

---
+++ PrG1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = new Random().Next(10,100);
+Console.WriteLine($" рандомное число = {num}");
+int a1 = num/10;
+int a2 = num % 10;
+int max = a1;
+if (max < a2) 
+{
+    max = a2;
+}
+Console.WriteLine($"максимальное цифра в числе = {max}");
