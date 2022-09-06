b13e37799ce7f9ff99439845a69ca14e33c02f32
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-06 18:03:15 +0300

    исправлены ошибки в PrG7

---
+++ PrG7/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+if (a*a == b || b*b == a)
+{
+    Console.WriteLine($"a = {a}, b = {b}, -> да");
+}
+else
+{
+    Console.WriteLine($"a = {a}, b = {b}, -> нет");
+}
